2050b9509252ca37510903a55a53ac66b1e2b45f
Kladko Denys <kladko.denys@gmail.com>
2024-03-11 09:53:29 +0200

    Completed task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,33 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { BooksActions, BooksApiActions } from './state/books.actions';
+import { GoogleBooksService } from './book-list/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  counter = 0;
-  updateAt?: number;
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
 
-  get cannotDecrease(): boolean {
-    return this.counter <= 0;
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
   }
 
-  increase(): void {
-    this.updateAt = Date.now();
-    this.counter++;
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
   }
 
-  decease(): void {
-    this.updateAt = Date.now();
-    this.counter--;
-  }
+  constructor(private booksService: GoogleBooksService, private store: Store) {}
 
-  clear(): void {
-    this.updateAt = Date.now();
-    this.counter = 0;
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) =>
+        this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+      );
   }
 }
